b55a837a390812f2baf9627ffe82ad4034e68eac
Sereno L. Lopez-Darwin <sereno@princeton.edu>
2024-03-11 19:09:21 -0400

    added eid to symbol conversion

---
+++ env_setup_spatialstem.R
@@ -0,0 +1,36 @@
+# Close and re-open shell.
+# 	>mamba create -n spatialstem python=3.11
+#   >mamba activate spatialstem
+# Environment defaults
+#   >conda config --env --add channels defaults
+#   >conda config --env --add channels bioconda
+#   >conda config --env --add channels conda-forge
+#   >mamba install git pip
+# Packages
+#   >mamba install conda-forge::r-base=4.3.1
+#   >mamba install conda-forge::jupyterlab
+#   >mamba install conda-forge::ipykernel
+# Activate kernel in Python
+#   >python -m ipykernel install --user --name=spatialstem-py311
+#   >mamba install conda-forge::r-irkernel
+#   >mamba install conda-forge::gh
+#   >mamba install conda-forge::jupyter-resource-usage
+# 	>mamba install r-devtools
+# Activate kernel in R
+devtools::install_github("IRkernel/IRkernel")
+IRkernel::installspec(name='spatialstem431', displayname='SpatialStem 4.3.1')
+# Python packages
+#   >mamba install -c conda-forge scanpy python-igraph leidenalg
+#   >mamba install conda-forge::glob2
+#   >mamba install conda-forge::squidpy
+#   >mamba install conda-forge::anndata
+#   >mamba install conda-forge::matplotlib
+#   >mamba install conda-forge::numpy
+# Seurat prereqs, must be installed in mamba
+#   >mamba install conda-forge::r-igraph
+#   >mamba install conda-forge::r-leiden
+remotes::install_github("satijalab/seurat", "seurat5", quiet=TRUE)
+#   >mamba install conda-forge::r-biocmanager
+BiocManager::install("DropletUtils")
+install.packages("scCustomize")
+#   >mamba install conda-forge::mygene
